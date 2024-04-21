75916db2b7caa77882fe883861dca7664145cfa6
akramennaseh <akramennaseh01@gmail.com>
2024-04-21 13:35:39 +0100

    dgzgdz

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -149,7 +149,7 @@ const Page = () => {
             });
 
             const data = await response.json();
-            console.log(data);
+            console.log(JSON.stringify(data, null, 2));
             console.log("Received data from API:", data);
             console.log("Received sessionId from API:", data.sessionurl);
             toast({ description: "Data submitted successfully." });
