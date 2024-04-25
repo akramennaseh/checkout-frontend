fb8fc597b3c2ce3a42d64d78f2ea7c28e80cf3ce
akramennaseh <akramennaseh01@gmail.com>
2024-04-25 21:59:15 +0100

    dgzgdz

---
+++ components/footerEmail.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FooterEmail = () => {
+    return (
+        <div data-v-a637fb04="" className="footer bg-gradient-to-r from-white to-blue-zodiac text-blue-zodiac" >
+    <div data-v-a637fb04="" className="container" >
+        <div data-v-a637fb04="" className="sub-footer border-t border-malibu flex text-lg justify-between leading-normal py-7" style={{ textAlign: "center", display: "block" }} >
+            <p data-v-a637fb04="" className="copyright">
+                © 2024 - BesterTv - support@bestertv.com
+            </p>
+        </div>
+    </div>
+</div>
+    )
+}
+
+export default FooterEmail
